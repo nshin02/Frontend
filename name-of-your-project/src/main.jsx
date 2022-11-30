aab2203db171db492d395d390fb5e0bc724d31e8
Derrick Cui <derrick_cui@berkeley.edu>
2022-11-30 00:55:52 -0800

    stable

---
+++ name-of-your-project/src/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import Root from "./routes/root";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // Route,
+} from "react-router-dom";
+import "./index.css";
+import { Splash } from "./visualization";
+// import { Visualization } from "./visualization";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Splash />,
+  },
+
+  // {
+  //   path: "/login",
+  //   element: <Visualization />,
+  // },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
